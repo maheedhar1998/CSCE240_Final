c0c02978187599abe24287a8c6abfec9c7d1c92d
asanamandra <asanamandra@gmail.com>
2017-12-08 18:31:44 -0500

    Add files via upload

--- Human.cpp
+++ Human.cpp
@@ -1,4 +1,5 @@
 #include <cstdlib>
+#include <iostream>
 #include "Human.h"
 
 
@@ -6,21 +7,98 @@ using namespace std;
 
 Human::Human()
 {
-  x = 0;
-  y = 0; 
-  health = 100;
+ 	x = 0;
+ 	y = 0; 
+	health = 100;
+	Weapon = "none";
+	
 }
 Human::Human(int xVal, int yVal)
 {
-    
+	setX(xVal);
+	setY(yVal);
+	setHealth((rand()%100)+1);
+	String a = weaponArr[rand()%14];
+	weapon = new Weapon(a);
 }
 Human::Human(Human& human)
 {
+	
   
+}
+Human::~Human()
+{
 }
 int Human::getX()
 {
-   
+	return x;
 }
+int Human::getY()
+{
+	return y;
+}
+int Human::getHealth()
+{
+	return health;
+}
+Weapon Human::getWeapon()
+{
+	return weapon;
+}
+void Human::setX(int _x)
+{
+	if(x <= 0 || x > 8)
+	{
+		cout << "Index out of bounds" << endl;
+	}
+	else
+	{
+		x = _x;
+	}
+}
+void Human::setY(int _y)
+{
+	if(y <= 0 || y > 8)
+	{
+		cout << "Index out of bounds" << endl;
+	}
+	else
+	{
+		y = _y;
+	}
+}
+void Human::setHealth(int a)
+{
+	if(x <= 0 || x > 100)
+	{
+		cout << "Invalid Health!" << endl;
+	}
+	else
+	{
+		health = a;
+	}
+}
+void Human::increaseHealth(int b)
+{
+	
+	health = health+b;
+
+	if(health > 100)
+	{
+		health = 100;
+	}
+	
+}
+void Human::decreaseHealth(int c)
+{
+	health = health-c;
+
+	if(health <= 0)
+	{
+		health = 0;
+	}
+}
+
+
 
 
--- Human.h
+++ Human.h
@@ -8,6 +8,7 @@ class Human
         Human();
         Human(int,int);
         Human(Human&);
+	~Human();
         int getX();
         int getY();
         int getHealth();
@@ -18,9 +19,13 @@ class Human
         void increaseHealth(int);
         void decreaseHealth(int);
         void setWeapon();
+	
+	
+	
     private:
         int x;
         int y;
         int health;
+	string weaponArr[14] = {"Machine Gun","Baseball Bat","Machete","Katana","Axe","Zanpakto","Trench Knife","Crowbar","Crossbow","Bow & Arrow","Mace","Grenades","Shot Gun","Assault Rifle"};
         Weapon weapon;
 };
